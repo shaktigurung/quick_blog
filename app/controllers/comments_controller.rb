17cf468015299f299726871c1100b13f67268dcb
shaktigurung <mr.shakti.gurung@gmail.com>
2018-10-30 11:26:19 +1100

    Ajax added for comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,11 @@
 class CommentsController < ApplicationController
     def create
-        @post = Post.find(params[:post_id])
-        @comment = @post.comments.create!(comment_params)
-        redirect_to @post
+      @post = Post.find(params[:post_id])
+      @comment = @post.comments.create!(comment_params)
+      respond_to do |format|
+        format.html { redirect_to @post }
+        format.js { redirect_to @post }
+      end
       end
     
       private
